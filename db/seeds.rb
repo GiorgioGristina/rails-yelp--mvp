c3997065e013bdb33707fcf15180b4d2fd942577
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-04-11 10:59:22 +0100

    created restaurant and review model and controller  with related validation: a restaurant must have name, category and address; when a restaurant is destroyed also the reviews mus be destroyed as well; review belong to a restaurant; review must have content and rating; a review's rating must be a number betweeen 0 and 5; in restaurant controller added index and show action and related view diplaying related content on it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "Created id: #{restaurant.id} name:#{restaurant.name}"
+end
+
+puts "Finished!"
