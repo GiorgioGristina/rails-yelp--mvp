9ed75d57d7b4d20b772ca23156de76511fe443f9
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-04-11 11:47:58 +0100

    add to restaurant controller action show @review instance so i can display the form to create the review in the restaurant show page and in the review controller the action create to create the review

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,9 +5,9 @@ class RestaurantsController < ApplicationController
     
   end
 
-  def show 
-    
+  def show     
     @restaurant= Restaurant.find(params[:id])
+    @review = Review.new
   end
 
   def new
@@ -15,7 +15,18 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else 
+      render :new
+    end
 
-    
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
